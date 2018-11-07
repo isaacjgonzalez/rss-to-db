9ad6b387cc8f04ab74da4e4039169fde58fce53b
Isaac Gonzalez <isaacjgonzalez@cilenis.com>
2018-11-07 11:54:15 +0100

    Created folders for testing data and for test scripts

---
+++ test_data/insert_blogomillo_in_db.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Init db with values un test_threads
+
+import csv
+from pymongo import MongoClient
+import re
+from urllib.parse import urlparse
+
+DB_NAME = "feeds"
+COLLECTION_NAME_SOURCES = "0_sources"
+
+client = MongoClient('localhost', 27017)[DB_NAME]
+
+
+with open('blogomillo.txt') as file:
+  for line in file:
+      parsed_uri = urlparse(line)
+      name = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+
+      row = {"name":name,"url":line}
+      #print(row)
+      insertion_code = client[COLLECTION_NAME_SOURCES].insert(row)
+      print(insertion_code)
