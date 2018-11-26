5952ed4fcb88af43ba0ebdba08d9362c5e0c665f
Isaac Gonzalez <isaacjgonzalez@cilenis.com>
2018-11-26 11:50:26 +0100

    Basic loggin system with print in one line

--- rss_to_db.py
+++ rss_to_db.py
@@ -24,8 +24,10 @@ def struct_time_to_timestamp(struct_time):
 class Feed:
 	def __init__(self,feed_info,db):
 		self.info = feed_info
+		self.info["name"] = self.info["name"].strip()
 		self.db = db
 		self.items = []
+		self.errors = []
 		#feedparser.USER_AGENT = "MyApp/1.0 +http://example.com/"
 		if not "etag" in self.info.keys():
 			self.info["etag"] = ""
@@ -35,15 +37,18 @@ class Feed:
 			self.info["last_time_item"] = 0
 
 	def run(self):
-		self.print_feed_name()
+		#.print_feed_name()
 		fetch_code = self.fetch_rss()
 		if fetch_code <0:
 			return -1
 		number_of_items = self.parse_rss()
 		if number_of_items > 0:
 			self.store()
-			self.print_titles()
+			#.print_titles()
 		self.update_feed_info()
+		print(self.create_log_info())
+		if len(self.errors)>0:
+			print(self.create_log_error())
 		return number_of_items
 
 	def fetch_rss(self):
@@ -51,7 +56,11 @@ class Feed:
 			self.feed = feedparser.parse(self.info["url"],self.info["etag"],self.info["modified"])
 			status = self.feed.status
 		except:
-			print(" Error in feedparser: ",self.feed)
+			self.errors.append(" Feedparser:"+str(self.feed))
+			return -1
+		if self.feed.bozo == 1:
+			# XML not well-formed, consider to delete from Sources
+			self.errors.append(" Bozo")
 			return -1
 
 		if status == 200:
@@ -63,23 +72,24 @@ class Feed:
 
 		elif status == 302 or status == 301:
 			self.info["url"] = self.feed.href
-			print(" Url moved to: ",self.feed.href,". Restart run feed")
+			self.errors.append(" Url moved to:"+self.feed.href+". Restart run feed")
 			self.update_feed_info()
 			self.fetch_rss()
 			return 0
 		elif status == 304:
 			# feed with nothing new
-			print(" Nothing new in ",self.info["name"],", etag status: ",self.feed.status)
+			#print(" Nothing new in ",self.info["name"],", etag status: ",self.feed.status)
 			return 0
 		elif status == 401:
 			# feedgone, remove from db
+			self.errors.append(" Url gone (401)")
 			pass
 			return -1
 		elif status == 404:
 			# error accessing url, check several times and remove if it's persists
 			return -1
 		else:
-			print( "HTTP Error not handled: ",status)
+			self.errors.append( "HTTP Error not handled: "+str(status))
 			return -1
 
 		return 1
@@ -93,30 +103,31 @@ class Feed:
 				item["published_at_time"] = struct_time_to_timestamp(entry.published_parsed)
 				# If item is not new, skip
 				if self.info["last_time_item"] >= item["published_at_time"]:
-					print("  Item already in db")
+					#print("  Item already in db")
 					continue
 				else:
 					if new_last_time_item <  item["published_at_time"]:
 						new_last_time_item =  item["published_at_time"]
 			except:
-				print("  Error in published_parsed! Check if content is not new can't be done!")
+				self.errors.append(" Error in published_parsed!")
 
 			try:
 				item.update({"title" : entry.title, "link" : entry.link , "saved_at" : datetime.now()})
 			except:
-				print("  Error in title, or link, or author")
+				self.errors.append(" Error in title or link")
 			try:
 				item["author"] = entry.author
 			except:
-				print("  Error in author")
+				item["author"] = ""
+				self.errors.append(" Error in author")
 			try:
 				item["published_at_str"]  = entry.published
 			except:
-				print("  Error in published")
+				self.errors.append(" Error in published")
 			try:
 				item["content_html"] = entry.description
 			except:
-				print("  Error in content")
+				self.errors.append(" Error in content")
 
 			#item["content_text"] = html2text.html2text(item["content_html"])
 			self.items.append(item)
@@ -146,6 +157,14 @@ class Feed:
 
 	def print_feed_name(self):
 		print("\n",self.info["name"])
+
+	def create_log_info(self):
+		line = str(datetime.now()) + " - INFO: " + self.info["name"] + " - " + str(len(self.items)) + " new feeds. "
+		return line
+
+	def create_log_error(self):
+		line = str(datetime.now()) + " - ERROR: " + self.info["name"] + " - Errors: " + ", ".join(self.errors)
+		return line
 
 
 class DatabaseMongo:
